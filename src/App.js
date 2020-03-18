1da2eff947612d7e776b2e7a4543addea478f7d4
Dumindu <dumindu@ceylonsolutions.com>
2020-03-18 15:57:41 +0530

    Drag and drop initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const getItems = count =>
+    Array.from({ length: count }, (v, k) => k).map(k => ({
+        id: `item-${k}`,
+        content: `question ${k}`
+    }));
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    console.log(removed);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: grid,
+    width: 250
+});
 
 function App() {
+  const [items, setItems] = useState(getItems(10));
+
+  const onDragEnd = result => {
+        if (!result.destination) {
+            return;
+        }
+
+        const reordersItems = reorder(
+            items,
+            result.source.index,
+            result.destination.index
+        );
+
+        setItems(reordersItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', height: 'auto'}}>
+      <DragDropContext onDragEnd={result => onDragEnd(result)}>
+          <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                  <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={getListStyle(snapshot.isDraggingOver)}
+                  >
+                      {items.map((item, index) => (
+                          <Draggable key={item.id} draggableId={item.id} index={index}>
+                          {(provided, snapshot) => (
+                              <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  style={getItemStyle(
+                                      snapshot.isDragging,
+                                      provided.draggableProps.style
+                                  )}
+                              >
+                                  {item.content}
+                              </div>
+                          )}
+                          </Draggable>
+                      ))}
+                      {provided.placeholder}
+                  </div>
+              )}
+
+          </Droppable>
+      </DragDropContext>
     </div>
   );
 }
